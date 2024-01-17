ba22fdacbc4f3171c4e9821b8ac7f30c912f6ac2
Pedro Fogaça <pedrofogacami@gmail.com>
2024-01-17 16:08:54 -0300

    feat: :art: Improve structure to the code

--- api/sus.py
+++ api/sus.py
@@ -10,5 +10,19 @@ class SUS_API:
         self.base_url = 'https://imunizacao-es.saude.gov.br/_search'
 
     def get_api(self):
-        response = requests.get(self.base_url, auth=HTTPBasicAuth(self.username, self.password))
-        return response.text
+        try:
+            response = requests.get(self.base_url, auth=HTTPBasicAuth(self.username, self.password))
+            
+            if response.status_code == 200:
+                data = response.json()
+                return data
+            else:
+                print(f'Erro na solicitação!!! Código de Status: {response.status_code}')
+                return response.json()
+            
+        except Exception as e:
+            print(f'Erro ao acessar a API: {str:(e)}')    
+
+if __name__ == '__main__':
+    conn = SUS_API()
+    print(conn.get_api())
