412c314503c6bbe0b7597be423c43751a26ba42a
Pedro Fogaça <pedrofogacami@gmail.com>
2024-01-22 09:03:39 -0300

    docs: :memo: add docstrings

--- api/sus.py
+++ api/sus.py
@@ -3,12 +3,42 @@ from requests.auth import HTTPBasicAuth
 
 
 class SUS_API:
+    """
+    Classe para interagir com a API do SUS.
+
+    Attributes:
+        username (str) -> Nome de usuário para autenticação na API.
+        password (str) -> Senha para autenticação na API.
+        base_url (str) -> URL base da API.
+
+    Methods:
+        get_api() -> Realiza uma solicitação GET à API do SUS.
+    """
+
     def __init__(self, username, password):
+        """
+        Inicializa a instância da classe SUS_API.
+
+        Args:
+            username (str) -> Nome de usuário para autenticação na API.
+            password (str) -> Senha para autenticação na API.
+        """
+        
         self.username = username
         self.password = password
         self.base_url = 'https://imunizacao-es.saude.gov.br/_search'
 
     def get_api(self):
+        """
+        Realiza uma solicitação GET à API do SUS.
+
+        Returns:
+            requests.Response -> Objeto de resposta da solicitação à API.
+        Raises:
+            Exception -> Se a resposta da API não retornar um código de status 200.
+        """
+
+        print('requisição iniciada')
         try:
             response = requests.get(self.base_url, auth=HTTPBasicAuth(self.username, self.password))
             
@@ -23,5 +53,6 @@ class SUS_API:
 
 
 if __name__ == '__main__':
+    # Exemplo de uso da classe SUS_API
     conn = SUS_API()
     print(conn.get_api())
--- app/etl.py
+++ app/etl.py
@@ -8,12 +8,40 @@ from api.sus import SUS_API
 
 
 class ETL: 
+    """
+    Classe para realizar operações de Extração, Transformação e Carga (ETL) de dados.
+    
+    Atributes: 
+        None
+
+    Methods:
+        extract() -> Realiza extracação dos dados da API do SUS.
+        transform() -> Transforma os dados brutos retornados em um df.
+        load_to_bucket() -> Carrega um arquivo csv no Google Cloud Storage.
+    """
+   
     def extract():
+        """
+        Realiza a extração de dados da API do SUS.
+
+        Returns:
+            dict: Dados extraídos da API no formato de dicionário.
+        """
+            
         conn = SUS_API(os.getenv('USERNAME'), os.getenv('PASSWORD'))
         response = conn.get_api()
         return response.json()
     
-    def transform(data):
+    def transform(data:dict):
+        """
+        Realiza a transformação dos dados da API em um DataFrame e salva em um arquivo CSV.
+
+        Args:
+            data (dict): Dados extraídos da API no formato de dicionário.
+        Returns:
+            int: Retorna 1 para indicar que a transformação foi concluída.
+        """
+
         df_raw = pd.DataFrame()
 
         for i in range(len(data['hits']['hits'])):
@@ -34,10 +62,20 @@ class ETL:
             df_raw = pd.concat([df_raw, df_temp], ignore_index=True)
 
         df_raw.to_csv(r'.\data\sus_data.csv', index=False)
-        print(len(df_raw) + 'linhas foram salvas em .\data\sus_data.csv')
+        print(f'{len(df_raw)} linhas foram salvas em .\data\sus_data.csv')
         return 1
         
     def load_to_bucket(client):
+        """
+        Carrega o arquivo CSV gerado para um bucket no Google Cloud Storage.
+
+        Args:
+            client: Cliente do Google Cloud Storage.
+
+        Returns:
+            int: Retorna 1 para indicar que o carregamento foi concluído.
+        """
+
         bucket = client.get_bucket('stack-sus')
         blob = bucket.blob('sus_data.csv')
         blob.upload_from_filename(r'.\data\sus_data.csv')
@@ -51,6 +89,10 @@ class ETL:
 
 
 def main():
+    """
+    Função principal que executa as operações ETL.
+    """
+    
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'.\utils\coastal-fiber-411610-ff4ba4c97db9.json'
     client = storage.Client()
 
